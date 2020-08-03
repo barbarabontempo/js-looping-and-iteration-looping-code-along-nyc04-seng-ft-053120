544d6d3d6d2d255aa835f14e3f2577763794ddaa
BarbaraBontempo <barbarabontempo94@gmail.com>
2020-08-03 00:19:02 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+
+function writeCards(array, name) {
+    const tyCards = []
+    for (let i = 0; i < array.length; i++) {
+        tyCards.push(`Thank you, ${array[i]}, for the wonderful ${name} gift!`);
+    }
+    return tyCards;
+}
+
+function countDown(number) {
+    let countup = 0
+    while (countup <= number){
+        console.log(countup++)
+    }
+
+}
